3bca7eff54ce703be56332bbdb259ce55b8645a1
DCODEV1702 <32214072+dcodev1702@users.noreply.github.com>
2023-03-11 13:20:31 -0500

    Update create_custom_table.ps1
    Moved Azure Login / Module logic to helper_functions.ps1

--- create_custom_law_table/create_custom_table.ps1
+++ create_custom_law_table/create_custom_table.ps1
@@ -11,55 +11,6 @@ Usage: Import-AzLACustomeTable -Environment 'AzureCloud' `
 # This feature requires PS >= 4.0
 #Requires -RunAsAdministrator
 
-function Get-AzureSubscription($Environment) {
-
-    # Test to see if there's an active login session to the Azure tenant
-    # Returns null of no login session exists 
-    $context = Get-AzContext -ErrorAction SilentlyContinue
-    if(!$context) {
-        # Check to see if Resource Group specified exists within the provided Azure Subscription
-        Write-Host "No active Azure session found. Please run Connect-AzAccount to connect to Azure." -ForegroundColor Magenta
-        Write-Host "`r`nYou will be asked to log in to your Azure environment if a session does not already exist. `nGlobal Admin or Security Admin credentials are required. `nThis will allow the script to interact with Azure as required.`r`n" -BackgroundColor Magenta
-        Read-Host -Prompt "Press enter to continue or CTRL+C to quit the script"
-        $context = Connect-AzAccount -Environment $Environment -UseDeviceAuthentication
-     
-    } else {
-        Write-Host "Active Azure session found." -ForegroundColor Green
-    }
-    return ($context)
-}
-
-function Check-AzModules() {
-
-    # Make sure any modules we depend on are installed
-    # Credit to: Koos Goossens @ Wortell.
-    $modulesToInstall = @(
-        'Az.Accounts',
-        'Az.Compute'
-    )
-
-    Write-Host "Installing/Importing PowerShell modules..." -ForegroundColor Green
-    $modulesToInstall | ForEach-Object {
-        if (-not (Get-Module -ListAvailable $_)) {
-            Write-Host "  ┖─ Module [$_] not found, installing..." -ForegroundColor Green
-            Install-Module $_ -Force
-        } else {
-            Write-Host "  ┖─ Module [$_] already installed." -ForegroundColor Green
-        }
-    }
-
-    $modulesToInstall | ForEach-Object {
-        if (-not (Get-InstalledModule $_)) {
-            Write-Host "  ┖─ Module [$_] not loaded, importing..." -ForegroundColor Green
-            Import-Module $_ -Force
-        } else {
-            Write-Host "  ┖─ Module [$_] already loaded." -ForegroundColor Green
-        }
-    }
-}
-
-
-
 Function Import-AzLACustomeTable {
     [CmdletBinding()]
     param(
