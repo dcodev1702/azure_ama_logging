932ce326e6c9ecdb389194e0ddafda655e3bf83d
DCODEV1702 <32214072+dcodev1702@users.noreply.github.com>
2023-03-11 12:58:45 -0500

    Update create_custom_table.ps1
    Added additional Azure login logic
    Ensured Administrator access is present
    Added checks to ensure the right Az Modules are installed 
    Added checks for Get-AzAccessToken

--- create_custom_law_table/create_custom_table.ps1
+++ create_custom_law_table/create_custom_table.ps1
@@ -2,17 +2,69 @@
 Author: dcodev1702 & my AI Sidekick (ChatGPT)
 Date: 10 March 2023
 
-Usage: Import-AzLACustomTable -Environment 'AzureCloud' `
+Usage: Import-AzLACustomeTable -Environment 'AzureCloud' `
        -ResourceGroup 'myRG' -Workspace 'myWorkspace' `
        -TableName 'Apache2_AccessLog_CL' -SaveFile 'apache2_accesslog_table.json'
 
 #>
 
-Function Import-AzLACustomTable {
+# This feature requires PS >= 4.0
+#Requires -RunAsAdministrator
+
+function Get-AzureSubscription($Environment) {
+
+    # Test to see if there's an active login session to the Azure tenant
+    # Returns null of no login session exists 
+    $context = Get-AzContext -ErrorAction SilentlyContinue
+    if(!$context) {
+        # Check to see if Resource Group specified exists within the provided Azure Subscription
+        Write-Host "No active Azure session found. Please run Connect-AzAccount to connect to Azure." -ForegroundColor Magenta
+        Write-Host "`r`nYou will be asked to log in to your Azure environment if a session does not already exist. `nGlobal Admin or Security Admin credentials are required. `nThis will allow the script to interact with Azure as required.`r`n" -BackgroundColor Magenta
+        Read-Host -Prompt "Press enter to continue or CTRL+C to quit the script"
+        $context = Connect-AzAccount -Environment $Environment -UseDeviceAuthentication
+     
+    } else {
+        Write-Host "Active Azure session found." -ForegroundColor Green
+    }
+    return ($context)
+}
+
+function Check-AzModules() {
+
+    # Make sure any modules we depend on are installed
+    # Credit to: Koos Goossens @ Wortell.
+    $modulesToInstall = @(
+        'Az.Accounts',
+        'Az.Compute'
+    )
+
+    Write-Host "Installing/Importing PowerShell modules..." -ForegroundColor Green
+    $modulesToInstall | ForEach-Object {
+        if (-not (Get-Module -ListAvailable $_)) {
+            Write-Host "  ┖─ Module [$_] not found, installing..." -ForegroundColor Green
+            Install-Module $_ -Force
+        } else {
+            Write-Host "  ┖─ Module [$_] already installed." -ForegroundColor Green
+        }
+    }
+
+    $modulesToInstall | ForEach-Object {
+        if (-not (Get-InstalledModule $_)) {
+            Write-Host "  ┖─ Module [$_] not loaded, importing..." -ForegroundColor Green
+            Import-Module $_ -Force
+        } else {
+            Write-Host "  ┖─ Module [$_] already loaded." -ForegroundColor Green
+        }
+    }
+}
+
+
+
+Function Import-AzLACustomeTable {
     [CmdletBinding()]
     param(
         [Parameter(Mandatory=$true)]
-	[ValidateSet('AzureCloud','AzureUSGovernment')]
+        [ValidateSet('AzureCloud','AzureUSGovernment')]
         [string]$Environment,
         [Parameter(Mandatory=$false)]
         [string]$SaveFile=$null,
@@ -21,18 +73,29 @@ Function Import-AzLACustomTable {
         [Parameter(Mandatory=$false)]
         [string]$Workspace,
         [Parameter(Mandatory=$false)]
-        [string]$TableName
+        [string]$TableName,
+        [Parameter(Mandatory = $false)]
+        [Switch] $CheckAzModules = $false
     )
 
+    
     # Check if the user has an active Azure session
-    if(Get-AzContext -ErrorAction SilentlyContinue){
-        Write-Host "Active Azure session found." -ForegroundColor Green
-        $SubscriptionId = (Get-AzContext).Subscription.Id
+    # Use a flag -CheckAzModules to enable checking of required modules
+    if ($CheckAzModules) { Check-AzModules }
+
+    # Before querying Azure, ensure we are logged in
+    $AzContext = Get-AzureSubscription($Environment)
+
+    # Get Azure Access (JWT) Token for API Auth/Access 
+    if($AzContext.Environment.Name -eq 'AzureCloud') {
+        $resourceUrl = 'https://management.azure.com'
     } else {
-        Write-Host "No active Azure session found. Please run Connect-AzAccount to connect to Azure." -ForegroundColor Red
-        Connect-AzAccount -Environment $Environment -UseDeviceAuthentication
-        $SubscriptionId = (Get-AzContext).Subscription.Id
+        $resourceUrl = 'https://management.usgovcloudapi.net/'
     }
+
+    $token = (Get-AzAccessToken -ResourceUrl $resourceUrl).Token
+    $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+    $headers.Add("Authorization","Bearer $token")
 
     # Prompt the user for information
     if (-not $ResourceGroup) {
@@ -71,14 +134,15 @@ Function Import-AzLACustomTable {
     $tableParams.properties.schema.columns += $timeGenerated_
     $tableParams.properties.schema.columns += $rawData_
     
-    Write-Host "The mandatory fields `"TimeGenerated:dateTime`" and `"RawData:string `" have been added to your custom log (CL)." -ForegroundColor Magenta
-    Write-Host "How many columns would you like to add to your custom table: `"$TableName`"?" -ForegroundColor Yellow
+    Write-Host "The mandatory fields `"TimeGenerated:dateTime`" and `"RawData:string `" have already been added to your custom log (CL)." -ForegroundColor Magenta
+    Write-Host "How many columns do you want to add to custom table: `"$TableName`"?" -ForegroundColor Yellow
     $columnCount = Read-Host "value"
     
-    # Define the accepted data types
-    $dataTypes = "string", "dynamic", "dateTime", "bool", "float", "int"
-    
+
     for ($i = 1; $i -le $columnCount; $i++) {
+
+        # Define the accepted data types
+        $dataTypes = "string", "dynamic", "dateTime", "bool", "float", "int"
         
         Write-Host "Valid datatypes are: string, dynamic, dateTime, bool, float, and int" -ForegroundColor Yellow
         $columnName = Read-Host "Enter the column name for column $i"
@@ -113,7 +177,7 @@ Function Import-AzLACustomTable {
 
     $validInput = $false
     while (-not $validInput) {
-        Write-Host "Do you want to send your custom table to Log Analytics via the API? (Y/N)" -ForegroundColor Red
+        Write-Host "Do you want to send your custom table to Log Analytics via API? (Y/N)" -ForegroundColor Red
         $sendTable = Read-Host "Make your selection "
         if ($sendTable.ToLower() -eq "y" -or $sendTable.ToLower() -eq "n") {
             $validInput = $true
@@ -128,8 +192,10 @@ Function Import-AzLACustomTable {
         $Table = $tableParams | ConvertTo-JSON -Depth 32
         if ($sendTable.ToLower() -eq "y") {
         
+            # Need to add check to ensure Access Token is current before calling Invoke-AzRestMethod
+            $subscriptionId = $AzContext.Subscription.Id
             Invoke-AzRestMethod -Path "/subscriptions/$subscriptionId/resourcegroups/$ResourceGroup/providers/microsoft.operationalinsights/workspaces/$Workspace/tables/$($TableName)?api-version=2021-12-01-preview" -Method PUT -payload $Table
-            Write-Host "Table `"$TableName`" created and sent via REST API." -ForegroundColor Green
+            Write-Host "Table `"$TableName`" created and sent via RESTFul API." -ForegroundColor Green
         } else {
             Write-Host "Table `"$TableName`" created and $($pwd)\$SaveFile but not sent via the REST API." -ForegroundColor Yellow
             Write-Output $Table
