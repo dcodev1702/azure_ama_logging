30b7ac13841573bcd2172e235e20a0e7ff4a3bda
DCODEV1702 <32214072+dcodev1702@users.noreply.github.com>
2023-04-08 15:40:09 -0400

    Update upload_dcr.ps1

--- create_custom_law_table/upload_dcr.ps1
+++ create_custom_law_table/upload_dcr.ps1
@@ -54,12 +54,8 @@ Function Upload-AzDataCollectionRule {
     $AzContext = Get-AzureSubscription($Environment)
     $SubscriptionId = $AzContext.Subscription.Id
 
-    # Get Azure Access (JWT) Token for API Auth/Access 
-    if($AzContext.Environment.Name -eq 'AzureCloud') {
-        $resourceUrl = 'https://management.azure.com'
-    } else {
-        $resourceUrl = 'https://management.usgovcloudapi.net/'
-    }
+    # Get the correct REST API Endpoint for Resource Management
+    $resourceUrl = (Get-AzContext).Environment.ResourceManagerUrl
 
     # API Auth for Invoke-AzRestMethod
     $token = (Get-AzAccessToken -ResourceUrl $resourceUrl).Token
