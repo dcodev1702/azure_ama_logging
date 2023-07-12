2e3cef4d9589af6c9b34211d9f97a2b657751afc
DCODEV1702 <32214072+dcodev1702@users.noreply.github.com>
2023-07-12 11:50:47 -0400

    Update sample_cef_msg.sh

--- cef/sample_cef_msg.sh
+++ cef/sample_cef_msg.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-logger -p local5.info --rfc3164 --tcp -t CEF "0|MFCC-DCO|CYBER-DTF|17.02.1775|Remote Executable Code was Detected|Advanced S-CPT Warfare Planner Maj JJ Bottles detected|100|src=199.3.6.26 spt=3082 dst=20.14.8.24 dpt=1702" -P 514  -n 127.0.0.1
+logger -p local5.info --rfc3164 --tcp -t CEF "0|MFCC-DCO|CYBER-DTF|17.05.1775|Remote Executable Code was Detected|Advanced S-CPT Warfare Planner Maj JJ Bottles detected|100|src=199.3.6.26 spt=3082 dst=20.14.8.24 dpt=1705" -P 514  -n 127.0.0.1
