f16b1e4188a852b74dea9bfe719a0ca2cbb527bb
DCODEV1702 <32214072+dcodev1702@users.noreply.github.com>
2024-01-31 13:59:21 -0500

    Update sample_cef_event.sh

--- cef/sample_cef_event.sh
+++ cef/sample_cef_event.sh
@@ -3,7 +3,7 @@
 PORT=514
 IP=192.168.10.224
 
-# Array of base log messages
+# Array of CEF log messages
 log_messages=(
     "0|MFCC-DOG|CYBER-DTF|17.02.1775|Remote Executable Code was Detected|Advanced 651 CPT TL (Capt GNU Compiler) detected|100|src=192.168.10.170 spt=46117 dst=20.14.8.24 dpt=1702"
     "0|MFCC-DOG|CYBER-DTF|17.99.1775|Remote Executable Code was Detected|Advanced 651 CPT SEL (MSGT Cloud Forensicator) detected|100|src=192.168.10.170 spt=46117 dst=20.14.8.24 dpt=1799"
