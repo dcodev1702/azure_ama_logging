af2f52635dcbb924ec6e0be76638cec49beeec43
DCODEV1702 <32214072+dcodev1702@users.noreply.github.com>
2024-04-11 17:31:20 -0400

    Update EntraID-Assessment.ps1

--- data_collection_rules/create/EntraID-Assessment.ps1
+++ data_collection_rules/create/EntraID-Assessment.ps1
@@ -28,7 +28,11 @@ $dceName     = "oda-dcr-endpoint"
 $dcrName     = "oda-dcr-rule"
 $customTable = "ODAStream_CL"
 
-# Create a custom log table in the LAW
+# ------------------------------------------------------------
+# Create a custom log (table) in a Log Analytics Workspace
+#
+# https://learn.microsoft.com/en-us/rest/api/loganalytics/tables/create-or-update?view=rest-loganalytics-2023-09-01&tabs=HTTP
+# ------------------------------------------------------------
 $customTablePayload = @"
 {
     "properties": {
@@ -53,8 +57,8 @@ $customTablePayload = @"
 "@
 
 # Check to see if the custom table already exists. If it does, do nothing. If it does not, create it.
-# https://learn.microsoft.com/en-us/rest/api/loganalytics/tables/create-or-update?view=rest-loganalytics-2023-09-01&tabs=HTTP
 $CreateCustomTable = "$((Get-AzContext).Environment.ResourceManagerUrl)/subscriptions/$((Get-AzContext).Subscription.Id)/resourceGroups/$resourceGroup/providers/Microsoft.OperationalInsights/workspaces/$workspaceName/tables/$customTable"
+
 $CTCheck = Invoke-AzRestMethod ($CreateCustomTable+"?api-version=2022-10-01") -Method GET
 
 if ($CTCheck.StatusCode -eq 200) {
@@ -66,7 +70,9 @@ if ($CTCheck.StatusCode -eq 200) {
 
 Start-Sleep -Seconds 1
 
+# ------------------------------------------------------------
 # Create the Data Collection Endpoint (DCE)
+# ------------------------------------------------------------
 $dcePayload = @"
 {
     "location": "$location",
@@ -78,10 +84,10 @@ $dcePayload = @"
 }
 "@
 
-
 # Check to see if DCE already exists. If it does, do nothing. If it does not, create it.
 # https://learn.microsoft.com/en-us/rest/api/monitor/data-collection-endpoints/get?view=rest-monitor-2022-06-01&tabs=HTTP
 $DCEResourceId = "$((Get-AzContext).Environment.ResourceManagerUrl)/subscriptions/$((Get-AzContext).Subscription.Id)/resourceGroups/$resourceGroup/providers/Microsoft.Insights/dataCollectionEndpoints/$dceName"
+
 $dceExists = Invoke-AzRestMethod ($DCEResourceId+"?api-version=2022-06-01") -Method GET
 
 if ($dceExists.StatusCode -eq 200) {
@@ -94,15 +100,18 @@ if ($dceExists.StatusCode -eq 200) {
 Start-Sleep -Seconds 1
 
 
-# Get Log Analytics Workspace Resource Id
+# ------------------------------------------------------------
+# Get the LAW & DCE Resource Ids
+#
 # https://learn.microsoft.com/en-us/rest/api/loganalytics/workspaces/get?view=rest-loganalytics-2023-09-01&tabs=HTTP
+# ------------------------------------------------------------
 $LAWResourceId = "$((Get-AzContext).Environment.ResourceManagerUrl)/subscriptions/$((Get-AzContext).Subscription.Id)/resourceGroups/$resourceGroup/providers/Microsoft.OperationalInsights/workspaces/$workspaceName"
+
 $LAWResult = Invoke-AzRestMethod ($LAWResourceId+"?api-version=2023-09-01") -Method GET
 
 # Get the LAW Resource Id
 $LAWResourceID = $LAWResult.Content | ConvertFrom-JSON
 Write-Host "LAW Resource Id: $($LAWResourceId.id)" -ForegroundColor Yellow
-
 
 # Get the DCE Resource Id
 $DCEResult = Invoke-AzRestMethod ($DCEResourceId+"?api-version=2022-06-01") -Method GET
@@ -110,7 +119,12 @@ $DCEResult = Invoke-AzRestMethod ($DCEResourceId+"?api-version=2022-06-01") -Met
 $DCEResourceID = $DCEResult.Content | ConvertFrom-JSON
 Write-Host "DCE Resource Id: $($DCEResourceId.id)" -ForegroundColor Yellow
 
+
+# ---------------------------------------------------------------------------------
 # Create the data collection rule (DCR), linking the DCE and the LAW to the DCR
+#   
+# https://learn.microsoft.com/en-us/rest/api/monitor/data-collection-rules/create?view=rest-monitor-2022-06-01&tabs=HTTP
+# ---------------------------------------------------------------------------------
 $dcrPayload = @"
 {
     "location": "$location",
@@ -175,8 +189,8 @@ $dcrPayload = @"
 "@
 
 # Check to see if DCR already exists. If it does, do nothing. If it does not, create it.
-# https://learn.microsoft.com/en-us/rest/api/monitor/data-collection-rules/create?view=rest-monitor-2022-06-01&tabs=HTTP
 $DCRResourceId = "$((Get-AzContext).Environment.ResourceManagerUrl)/subscriptions/$((Get-AzContext).Subscription.Id)/resourceGroups/$resourceGroup/providers/Microsoft.Insights/dataCollectionRules/$dcrName"
+
 $dcrExists = Invoke-AzRestMethod ($DCRResourceId+"?api-version=2022-06-01") -Method GET
 
 Start-Sleep -Seconds 1
