8099993f916f565cf729036962b8211d7ec6ab08
DCODEV1702 <32214072+dcodev1702@users.noreply.github.com>
2024-05-03 07:31:44 -0400

    Update Create-AMA-WindowsFirewallLog-API.ps1

--- data_collection_rules/windows_firewall_log/Create-AMA-WindowsFirewallLog-API.ps1
+++ data_collection_rules/windows_firewall_log/Create-AMA-WindowsFirewallLog-API.ps1
@@ -111,6 +111,25 @@ function Invoke-WindowsFW-API {
     # Delete Azure resources (Custom Table, DCE, & DCR) if the $Action is "Delete"
     if ($Action -eq "Delete") {
         try {
+            # Get data collection rule associations and remove associated VM's (Resources)
+            $VMResources = Get-AzDataCollectionRuleAssociation -DataCollectionRuleName $dcrName -ResourceGroupName $ResourceGroup 
+
+            foreach ($VMResource in $VMResources) {  
+                $parts = $VMResource.Id -split '/'
+                $RType = "$($parts[6])/$($parts[7])"    
+                $vmName = $parts[8]
+
+                # Get the VM resource
+                $VM = Get-AzResource -ResourceGroupName $RGroupName -Name $vmName -ResourceType $RType
+
+                if ($vmResource) {
+                    # Output the resource id
+                    Remove-AzDataCollectionRuleAssociation -AssociationName $VMResource.Name -ResourceUri $VM.Id
+                } else {
+                    Write-Warning "VM resource '$vmName' with resource type '$RType' not found in resource group '$RGroupName'."
+                }
+            }
+
             Set-AzResource -Resource_API $LATable_API -ResourceName $customTable
             Set-AzResource -Resource_API $DCR_API -ResourceName $dcrName
             Set-AzResource -Resource_API $DCE_API -ResourceName $dceName
