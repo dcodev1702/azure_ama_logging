21f6fc580706885235bde8af7f4b5e2fba75a9f7
DCODEV1702 <32214072+dcodev1702@users.noreply.github.com>
2025-02-26 11:57:36 -0500

    Update sample_cef_event.sh

--- cef/sample_cef_event.sh
+++ cef/sample_cef_event.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-PORT=514
-IP=192.168.10.224
+SYSLOG_PORT=514
+IPAddress=$(hostname -I | awk '{print $1}')
 
 # Array of CEF log messages
 log_messages=(
-    "0|MFCC-DOG|CYBER-DTF|17.02.1775|Remote Executable Code was Detected|Advanced 651 CPT TL (Capt GNU Compiler) detected|100|src=192.168.10.170 spt=46117 dst=20.14.8.24 dpt=1702"
-    "0|MFCC-DOG|CYBER-DTF|17.99.1775|Remote Executable Code was Detected|Advanced 651 CPT SEL (MSGT Cloud Forensicator) detected|100|src=192.168.10.170 spt=46117 dst=20.14.8.24 dpt=1799"
-    "0|MFCC-DOG|CYBER-DTF|17.05.1775|Remote Executable Code was Detected|Advanced 651 CPT CWP (Maj JJ Bottles) detected|100|src=192.168.10.170 spt=46117 dst=20.14.8.24 dpt=1705"
+    "0|MFCC-DOG|CYBER-DTF|17.02.1775|Remote Executable Code was Detected|Advanced 651 CPT TL (Capt GNU Compiler) detected|100|src=$IPAddress spt=46117 dst=20.14.8.24 dpt=1702"
+    "0|MFCC-DOG|CYBER-DTF|17.99.1775|Remote Executable Code was Detected|Advanced 651 CPT SEL (MSGT Cloud Forensicator) detected|100|src=$IPAddress spt=46117 dst=20.14.8.24 dpt=1799"
+    "0|MFCC-DOG|CYBER-DTF|17.05.1775|Remote Executable Code was Detected|Advanced 651 CPT CWP (Maj JJ Bottles) detected|100|src=$IPAddress spt=46117 dst=20.14.8.24 dpt=1705"
 )
 
 # Function to log a message
 log_message() {
     local message="$1"
-    logger -p local5.info --rfc3164 --tcp -t CEF "$message" -P $PORT -n $IP
+    logger -p local5.info --rfc3164 --tcp -t CEF "$message" -P $SYSLOG_PORT -n $IPAddress
 }
 
 # Iterate over the array and log each message
