bd53e41d757d90325c1b999ddef553f16454b29b
DCODEV1702 <32214072+dcodev1702@users.noreply.github.com>
2025-02-26 11:58:52 -0500

    Update sample_cef_single_msg.sh

--- cef/sample_cef_single_msg.sh
+++ cef/sample_cef_single_msg.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-logger -p local5.info --rfc3164 --tcp -t CEF "0|MFCC-DCO|CYBER-DTF|17.05.1775|Remote Executable Code was Detected|Advanced S-CPT Warfare Planner Maj JJ Bottles detected|100|src=199.3.6.26 spt=3082 dst=20.14.8.24 dpt=1705" -P 514  -n 127.0.0.1
+SYSLOG_PORT=514
+IPAddress=$(hostname -I | awk '{print $1}')
+
+logger -p local5.info --rfc3164 --tcp -t CEF "0|MFCC-DCO|CYBER-DTF|17.05.1775|Remote Executable Code was Detected|Advanced S-CPT Warfare Planner Maj JJ Bottles detected|100|src=199.3.6.26 spt=3082 dst=20.14.8.24 dpt=1705" -P $SYSLOG_PORT  -n "$IPAddress"
